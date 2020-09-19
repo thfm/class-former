93417e1ac656d00ddd826210abcb4f77de314537
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-09-20 09:29:26 +1000

    Fill in `formClasses` function

--- script.js
+++ script.js
@@ -27,5 +27,43 @@ class Class {
 }
 
 function formClasses(course, teachers, students) {
+    // If there are no students or no teachers, there can't possibly be any classes
+    if (teachers.length == 0 || students.length == 0) {
+        return [];
+    }
 
+    let classes = [];
+
+    let baseNumOfStudentsPerClass = Math.floor(students.length / teachers.length);
+    let numLeftoverStudents = students.length % teachers.length;
+
+    let idx = 0;
+
+    for (let t of teachers) {
+        let numStudentsInThisClass = baseNumOfStudentsPerClass;
+
+        // If there are leftover students, take one of them and add them to the current class
+        if (numLeftoverStudents > 0) {
+            numLeftoverStudents--;
+            numStudentsInThisClass++;
+        }
+
+        classes.push(new Class(course, t, students.slice(idx, idx + numStudentsInThisClass)));
+        idx += numStudentsInThisClass;
 }
+
+    for (let c of classes) {
+        if (c.students.length < course.minStudents) {
+            // If there is an excess of teachers, remove one teacher and try forming classes again
+            teachers.pop();
+            return formClasses(course, teachers, students);
+        } else if (c.students.length > course.maxStudents) {
+            // If there is instead an excess of students, remove one student and try forming classes again
+            students.pop();
+            return formClasses(course, teachers, students);
+        }
+    }
+
+    return classes;
+}
+
